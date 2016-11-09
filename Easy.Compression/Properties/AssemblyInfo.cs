df6175124240ed6d9d7f277462d4d704ef2b9dcb
Nima Ara <NimaAra@users.noreply.github.com>
2016-11-09 20:15:44 +0000

    Update AssemblyInfo.cs

--- Easy.Compression/Properties/AssemblyInfo.cs
+++ Easy.Compression/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Easy.Compression")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("Easy.Compression.Tests.Unit")]
